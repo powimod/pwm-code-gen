dd7d6afcd3add1a5d2d28b569541d62d479f37f8
Dominique Parisot <powimod@gmail.com>
2023-11-14 06:18:46 +0100

    Control project validity

--- main.js
+++ main.js
@@ -26,11 +26,63 @@ function controlProject(project, verbose) {
 
 	if (project.objects === undefined)
 		throw new Error(`Project object list is not defined`);
+	if (project.objects.length === undefined)
+		throw new Error(`Project object list is not a list`);
+	if (project.objects.length === 0)
+		throw new Error(`Project object list is empty`);
 	if (verbose)
 		console.log(`Object list :`);
+	let i = 0;
 	for (let projectObject of project.objects){
+		i++;
+		if (projectObject.name === undefined)
+			throw new Error(`Name is not defined in object n°${i}`);
+		if (projectObject.type === undefined)
+			projectObject.type = 'string';
 		if (verbose)
-			console.log(`- Object ${projectObject.name}`);
+			console.log(`- Object ${projectObject.name} (type:${projectObject.type})`);
+
+		if (projectObject.properties === undefined)
+			throw new Error(`Property list is not defined in object <${projectObject.name}>`);
+		if (projectObject.properties.length === undefined)
+			throw new Error(`Property list is not a list in object <${projectObject.name}>`);
+		if (projectObject.properties.length === 0)
+			throw new Error(`Property list is empty in object <${projectObject.name}>`);
+		if (verbose)
+			console.log(`  Property list :`);
+		let j = 0;
+		for (let objectProperty of projectObject.properties){
+			j++;
+			if (objectProperty.name === undefined)
+				throw new Error(`Name of property n°${j} of object <${projectObject.name}> is not defined`);
+			if (objectProperty.type === undefined)
+				objectProperty.type = 'string';
+			if (verbose)
+				console.log(`    - Property ${objectProperty.name} (type:${objectProperty.type})`);
+		}
+	}
+
+	if (project.files === undefined)
+		throw new Error(`Project file list is not defined`);
+	if (project.files.length === undefined)
+		throw new Error(`Project file list is not a list`);
+	if (project.files.length === 0)
+		throw new Error(`Project file list is empty`);
+	if (verbose)
+		console.log(`File list :`);
+	i = 0;
+	for (let projectFile of project.files){
+		i++;
+		if (projectFile.scope === undefined)
+			throw new Error(`Scope is not defined in file n°${i}`);
+		if (projectFile.input === undefined)
+			throw new Error(`Input is not defined in file n°${i}`);
+		if (projectFile.output === undefined)
+			throw new Error(`Output is not defined in file n°${i}`);
+		if (verbose)
+			console.log(`- File n°${i} (scope:${projectFile.scope})`);
+			console.log(`    -> input:${projectFile.input})`);
+			console.log(`    -> ouptut:${projectFile.output})`);
 	}
 
 }
@@ -61,13 +113,15 @@ async function main() {
 		process.exit(1);
 	}
 
+	// control project validity
 	try {
 		controlProject(project, verbose);
 	}
 	catch (error) {
-		console.error(`Error : ${error.message} project file <${error}> !`);
+		console.error(`Error : ${error.message} in project file <${projectFile}> !`);
 		process.exit(1);
 	}
+
 }
 
 main();
