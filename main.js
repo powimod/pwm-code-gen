7840b292bc41449170b644cbc55062ce59e36e85
Dominique Parisot <powimod@gmail.com>
2023-11-16 13:29:50 +0100

    Terminate rewriting project loading

--- main.js
+++ main.js
@@ -154,6 +154,80 @@ function loadProjectName(project, name) {
 	project.name = name;
 }
 
+function loadFileScope(file, scope) {
+	if (file.scope !== null)
+		throw new Error(`File scope already defined`);
+	scope = scope.trim();
+	if (scope.length === 0)
+		throw new Error(`File scope is empty`);
+	const scopeValidList = [ 'project', 'object' ];
+	if (! scope in scopeValidList)
+		throw new Error(`Invalid file scope <${scope}> (should be ${scopeValidList.join('/')})`);
+	file.scope = scope;
+}
+
+function loadFileInput(file, inputFile) {
+	if (file.input !== null)
+		throw new Error(`File input already defined`);
+	inputFile = inputFile.trim();
+	if (inputFile.length === 0)
+		throw new Error(`File inputFile is empty`);
+	file.input = inputFile;
+}
+
+function loadFileOutput(file, outputFile) {
+	if (file.output !== null)
+		throw new Error(`File output already defined`);
+	outputFile = outputFile.trim();
+	if (outputFile.length === 0)
+		throw new Error(`File outputFile is empty`);
+	file.output = outputFile;
+}
+
+function loadProjectFileList(project, filesDefinition) {
+	assert.ok(project !== undefined);
+	assert.ok(filesDefinition !== undefined);
+	if (filesDefinition.length === undefined)
+		throw new Error(`File list is not an array`);
+	if (project.files.length > 0)
+		throw new Error(`Files list already loaded`);
+
+	let iFile = 0;
+	for (let fileDefinition of filesDefinition) {
+		iFile++;
+		let file = {
+			scope: null,
+			input: null,
+			output: null,
+			project: project
+		};
+
+		for (let attrName in fileDefinition) {
+			let attrValue = fileDefinition[attrName];
+			switch (attrName) {
+				case 'scope':
+					loadFileScope(file, attrValue);
+					break;
+				case 'input':
+					loadFileInput(file, attrValue);
+					break;
+				case 'output':
+					loadFileOutput(file, attrValue);
+					break;
+				default:
+					throw new Error(`Unknown attribut <${attrName}>`);
+					break;
+			}
+		}
+		if (file.scope === null)
+			throw new Error(`Scope of file n°${iFile} is not defined`);
+		if (file.input === null)
+			throw new Error(`Input of file n°${iFile} is not defined`);
+		if (file.output === null)
+			throw new Error(`Output of file n°${iFile} is not defined`);
+		project.files.push(file);
+	}
+}
 function loadProjectStep1(project, projectDefinition, verbose)
 {
 	for (let attrName in projectDefinition) {
@@ -166,7 +240,7 @@ function loadProjectStep1(project, projectDefinition, verbose)
 				loadProjectObjectList(project, attrValue);
 				break;
 			case 'files':
-				// TODO implementation
+				loadProjectFileList(project, attrValue);
 				break;
 			default:
 				throw new Error(`Unknown attribut <${attrName}>`);
@@ -286,6 +360,13 @@ function dumpProject(project)
 			console.log(`${tab(6)}- Link <${objectLink.name}> (target:${objectLink.target.name}, mandatory:${objectLink.mandatory})`);
 		}
 	}
+	console.log(`${tab(2)}  Files : x${project.files.length}`);
+	let iFile = 1;
+	for (let file of project.files) {
+		console.log(`${tab(2)}- File n°${iFile++} (scope <${file.scope}>)`);
+		console.log(`${tab(2)}   - Input : <${file.input}>)`);
+		console.log(`${tab(2)}   - Output : <${file.output}>)`);
+	}
 	console.log('');
 }
 
@@ -294,7 +375,8 @@ function loadProject(projectDefinition, verbose)
 {
 	let project = {
 		name : null,
-		objects : [] 
+		objects : [],
+		files : [],
 	};
 	loadProjectStep1(project, projectDefinition, verbose);
 	loadProjectStep2(project, projectDefinition, verbose);
@@ -303,115 +385,7 @@ function loadProject(projectDefinition, verbose)
 	return project;
 }
 
-function controlProject(project, verbose) {
 
-	
-
-	if (project.name === undefined) 
-		throw new Error(`Project name is not defined`);
-	if (verbose)
-		console.log(`Project name : ${project.name}`);
-
-	if (project.objects === undefined)
-		throw new Error(`Project object list is not defined`);
-	if (project.objects.length === undefined)
-		throw new Error(`Project object list is not a list`);
-	if (project.objects.length === 0)
-		throw new Error(`Project object list is empty`);
-	if (verbose)
-		console.log(`Object list :`);
-	let i = 0;
-	for (let projectObject of project.objects){
-		i++;
-		if (projectObject.name === undefined)
-			throw new Error(`Name is not defined in object n°${i}`);
-		if (projectObject.type === undefined)
-			projectObject.type = 'string';
-		if (verbose)
-			console.log(`- Object ${projectObject.name} (type:${projectObject.type})`);
-
-		if (projectObject.properties === undefined)
-			throw new Error(`Property list is not defined in object <${projectObject.name}>`);
-		if (projectObject.properties.length === undefined)
-			throw new Error(`Property list is not a list in object <${projectObject.name}>`);
-		if (projectObject.properties.length === 0)
-			throw new Error(`Property list is empty in object <${projectObject.name}>`);
-		if (verbose)
-			console.log(`  Property list :`);
-		let j = 0;
-		for (let objectProperty of projectObject.properties){
-			j++;
-			if (objectProperty.name === undefined)
-				throw new Error(`Name of property n°${j} of object <${projectObject.name}> is not defined`);
-			if (objectProperty.type === undefined)
-				objectProperty.type = 'string';
-			if (verbose)
-				console.log(`    - Property ${objectProperty.name} (type:${objectProperty.type})`);
-		}
-
-		if (projectObject.links !== undefined) {
-			if (projectObject.links.length === undefined)
-				throw new Error(`Link list is not a list in object <${projectObject.name}>`);
-			if (projectObject.links.length === 0)
-				throw new Error(`Link list is empty in object <${projectObject.name}>`);
-			if (verbose)
-				console.log(`  Link list :`);
-			let j = 0;
-			for (let objectLink of projectObject.links){
-				j++;
-				if (objectLink.link === undefined)
-					throw new Error(`Target of link n°${j} of object <${projectObject.name}> is not defined`);
-				if (objectLink.mandatory === undefined)
-					objectLink.mandatory = false;
-				if (objectLink.mandatory !== true && objectLink.mandatory !== false)
-					throw new Error(`Mandatory attribut of link <${objectLink.link}> of object <${projectObject.name}> is is not a boolean`);
-				if (verbose)
-					console.log(`    - Link ${objectLink.link} (mandatory:${objectLink.mandatory})`);
-				// check target source
-				let sourceObject = null; 
-				for (let obj of project.objects) {
-					if (objectLink.link === obj.name) {
-						sourceObject = obj;
-						break;
-					}
-				}
-				console.log(sourceObject.name);
-				if (sourceObject === undefined) 
-					throw new Error(`Unknown target link <${objectLink.link}> of object <${projectObject.name}>`);
-				objectLink.sourceObject = sourceObject;
-				if (sourceObject.linkedObjects === undefined)
-					sourceObject.linkedObjects = [];
-				sourceObject.linkedObjects.push(projectObject);
-			}
-		}
-
-	}
-
-	if (project.files === undefined)
-		throw new Error(`Project file list is not defined`);
-	if (project.files.length === undefined)
-		throw new Error(`Project file list is not a list`);
-	if (project.files.length === 0)
-		throw new Error(`Project file list is empty`);
-	if (verbose)
-		console.log(`File list :`);
-	i = 0;
-	for (let projectFile of project.files){
-		i++;
-		if (projectFile.scope === undefined)
-			throw new Error(`Scope is not defined in file n°${i}`);
-		if (projectFile.input === undefined)
-			throw new Error(`Input is not defined in file n°${i}`);
-		if (projectFile.output === undefined)
-			throw new Error(`Output is not defined in file n°${i}`);
-		if (verbose) {
-			console.log(`- File n°${i} (scope:${projectFile.scope})`);
-			console.log(`    -> input:${projectFile.input})`);
-			console.log(`    -> ouptut:${projectFile.output})`);
-		}
-	}
-
-}
 
 async function generateFiles(project, verbose) {
 	const liquid = new Liquid();
@@ -470,7 +444,6 @@ async function main() {
 
 	// control project validity
 	try {
-		//controlProject(project, verbose);
 		loadProject(project, verbose);
 	}
 	catch (error) {
@@ -478,13 +451,13 @@ async function main() {
 		process.exit(1);
 	}
 
-	//try {
-		//await generateFiles(project, verbose)
-	//}
-	//catch (error) {
-	//	console.error(`Error : ${error.message} in project file <${projectFile}> !`);
-	//	process.exit(1);
-	//}
+	try {
+		await generateFiles(project, verbose)
+	}
+	catch (error) {
+		console.error(`Error : ${error.message} in project file <${projectFile}> !`);
+		process.exit(1);
+	}
 
 	console.log('End.');
 }
